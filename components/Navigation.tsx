5ffce6ba3b4355c9144a94fb9d3bcae26fd98a7a
Ketarica <laridiajin@gmail.com>
2024-06-27 14:28:52 +0300

    test

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,39 +1,3 @@
-// import Image from "next/image";
-// export default function Navigation (){
-//     return (
-//         <div> 
-//             <p> Navigation </p>
-//         </div>
-//     );
-// }
-
-
-// import styles from '../app/nav.module.css';
-// const Navigation = () => {
-//   return (
-//     <nav className={styles.nav}>
-//       <div className={styles.logo}>
-//         <span className={styles.free}>free casino</span> <span className={styles.name}>FREEZINO</span>
-//       </div>
-//       <div className={styles.links}>
-//       {/* <div className={styles.buttonM}> */}
-//         <button className={styles.mainPage}>Main page</button>
-//         <button className={styles.moreGames}>More games</button>
-//         <button className={styles.myProfile}>My profile</button>
-//         <button className={styles.help}>Help</button>
-//         <button className={styles.work}>Work</button>
-//         <button className={styles.signUp}>Sing up</button>
-//         <button className={styles.logIn}>Log in</button>
-//         <div className={styles.icon}>
-//          {/* </div> */}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navigation;
-
 import styles from '../app/nav.module.css';
 
 const Navigation = () => {
